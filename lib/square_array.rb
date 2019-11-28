51241b7a46e599abb2baa462383b2589abc8c19b
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-11-28 19:20:42 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,9 @@
-
-
-def square_array(numbers)
-  numbers = [1,2,3]
-  counter = 0
-end
-
-while square_array(numbers) do
-  puts numbers(counter)**2
-  counter += 1
+def square_array(array)
+  squared = []
+  count = 0
+  while count < array.size do
+    puts squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
 end
