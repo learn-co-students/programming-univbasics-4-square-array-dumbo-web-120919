898eaa4ed09447d04d9a9340d82296ba3c6fcfaa
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-11-28 19:16:41 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+
+
+def square_array(numbers)
+  numbers = [1,2,3]
+  counter = 0
+end
+
+while square_array(numbers) do
+  puts numbers(counter)**2
+  counter += 1
 end
