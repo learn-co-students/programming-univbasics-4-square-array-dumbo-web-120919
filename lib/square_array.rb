b069c12a3eb1a270fae295d9d98e55f8543b92b9
Angelo Poole <angelo.poole@outlook.com>
2019-12-05 17:56:25 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,22 @@
+# def square_array(array)
+#   squared_array = []
+#   # count = 0
+#   array.each do|element|
+#     puts squared_array.push( element ** 2 )
+#     # count += 1
+#   end
+#   puts squared_array
+#   # your code here
+# end
+
+#solution taken from git
+
 def square_array(array)
-  # your code here
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
 end
